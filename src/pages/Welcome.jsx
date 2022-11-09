79bc00ee58d3e9e69eab37d9b370f65f9492edfc
ChrisCPH <christoffer221199@hotmail.com>
2022-11-09 14:46:52 +0100

    A few changes

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -7,6 +7,7 @@ function Welcome(props) {
             <h3>Welcome</h3>
             <p>To use the project follow the steps</p>
             <p> - Clone the backend project from git and follow the steps in that project</p>
+            <p> - npm install, npm run dev</p>
             <p> - Make sure you have an admin and user login in the database</p>
             <p> - Change the url link in apiFacade.js to fit your backend</p>
         </div>
